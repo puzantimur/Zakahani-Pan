29bcf435fb6147436e39fe2c8497a5671137783e
Timur Puzan <puzantimur1996@gmail.com>
2022-12-04 22:26:18 +0300

    added bottom navigation

---
+++ app/src/main/java/com/pan/FragmentContent.kt
@@ -0,0 +1,45 @@
+package com.pan
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.pan.databinding.FragmentContentBinding
+
+class FragmentContent : Fragment() {
+    private var _binding: FragmentContentBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentContentBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        with(binding) {
+            val nestedController =
+                (childFragmentManager.findFragmentById(R.id.page_container) as NavHostFragment)
+                    .navController
+            bottomNavigation.setupWithNavController(nestedController)
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
